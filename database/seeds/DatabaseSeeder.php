15da15fdd56cbcc37495836a217c3ed6a4d75d08
MP1NSRLX <bobby.gerez@yahoo.com>
2020-04-02 12:10:38 +0800

    Added Faker, Laravel Ui, Laravel Helpers

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(CompanyTableSeeder::class);
         $this->call(EmployeeTableSeeder::class);
     }
 }
